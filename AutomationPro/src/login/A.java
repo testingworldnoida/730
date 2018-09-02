4a353b7961704ff73c4b252bb7fe18a027119918
testingworldnoida <testingworldnoida@gmail.com>
2018-09-02 08:09:54 +0530

    JIRA-2 : Started Automation Project

---
+++ AutomationPro/src/login/A.java
@@ -0,0 +1,23 @@
+package login;
+
+public class A {
+	
+	public  A()
+	{
+		System.out.println("Welcome....");
+	}
+	
+	public int sum(int a, int b)
+	{
+		int c=a+b;
+		return c;
+	}
+	
+	
+	public void mul(int a, int b)
+	{
+		int c=a*b;
+		System.out.println(c);
+	}
+	
+}
---
+++ AutomationPro/src/registration/B.java
@@ -0,0 +1,13 @@
+package registration;
+
+import login.A;
+
+public class B {
+
+   public static void main(String[] args) {
+	
+	   A  o = new A();
+	  
+   }
+	
+}
